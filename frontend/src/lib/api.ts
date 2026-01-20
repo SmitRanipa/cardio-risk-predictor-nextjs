780ca9ea8d7f6d414fe3dc5529deeec26be0d855
SmitRanipa <smitranipa@gmail.com>
2026-01-20 09:18:06 +0530

    Fix API base env variable

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,8 @@
 // export const API_BASE = "http://localhost:8000";
 
 export const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
+
 
 export async function predictRisk(payload: any) {
   const res = await fetch(`${API_BASE}/predict`, {
